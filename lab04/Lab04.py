dcc8c10069427445d4a39d3719e87576586f6d70
JustADude29 <pavankumar82903@gmail.com>
2023-08-30 10:37:33 +0530

    balsack

--- lab04/Lab04.py
+++ lab04/Lab04.py
@@ -77,98 +77,98 @@ MG2,MGT2,rejects2 = MargBray(10000)
 print("Mean of 100 samples generated throught Margsalia and Bray method:", np.mean(MG1), "and variance:", np.var(MG1))
 print("Mean of 10000 samples generated throught Margsalia and Bray method:", np.mean(MG2), "and variance:", np.var(MG2))
 
-# plt.hist(BM1, 100, density=False)
-# plt.title("Histogram for BoxMuller method: 100 samples")
-# plt.show()
+plt.hist(BM1, 100, density=False)
+plt.title("Histogram for BoxMuller method: 100 samples")
+plt.show()
 
 
-# plt.hist(BM2, 100, density=False)
-# plt.title("Histogram for BoxMuller method: 10000 samples")
-# plt.show()
+plt.hist(BM2, 100, density=False)
+plt.title("Histogram for BoxMuller method: 10000 samples")
+plt.show()
 
 
-# plt.hist(MG1, 100, density=False)
-# plt.title("Histogram for Margsalia and Bray method: 100 samples")
-# plt.show()
+plt.hist(MG1, 100, density=False)
+plt.title("Histogram for Margsalia and Bray method: 100 samples")
+plt.show()
 
 
-# plt.hist(MG2, 100, density=False)
-# plt.title("Histogram for Margsalia and Bray method: 10000 samples")
-# plt.show()
+plt.hist(MG2, 100, density=False)
+plt.title("Histogram for Margsalia and Bray method: 10000 samples")
+plt.show()
 
-# data = np.array(BM1)
-# data = data*math.sqrt(5)
-# x = np.linspace(-10,10,10000)
-# dat2 = [Normal(i, 0, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for BoxMuller method: N=100, mean=0, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(BM1)
+data = data*math.sqrt(5)
+x = np.linspace(-10,10,10000)
+dat2 = [Normal(i, 0, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for BoxMuller method: N=100, mean=0, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(BM2)
-# data = data*math.sqrt(5)
-# x = np.linspace(-10,10,10000)
-# dat2 = [Normal(i, 0, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for BoxMuller method: N=10000, mean=0, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(BM2)
+data = data*math.sqrt(5)
+x = np.linspace(-10,10,10000)
+dat2 = [Normal(i, 0, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for BoxMuller method: N=10000, mean=0, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(MG1)
-# data = data*math.sqrt(5)
-# x = np.linspace(-10,10,10000)
-# dat2 = [Normal(i, 0, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for MargsaliaBray method: N=100, mean=0, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(MG1)
+data = data*math.sqrt(5)
+x = np.linspace(-10,10,10000)
+dat2 = [Normal(i, 0, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for MargsaliaBray method: N=100, mean=0, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(MG2)
-# data = data*math.sqrt(5)
-# x = np.linspace(-10,10,10000)
-# dat2 = [Normal(i, 0, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for MargsaliaBray method: N=10000, mean=0, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(MG2)
+data = data*math.sqrt(5)
+x = np.linspace(-10,10,10000)
+dat2 = [Normal(i, 0, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for MargsaliaBray method: N=10000, mean=0, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-#----------------------------
+# ----------------------------
 
-# data = np.array(BM1)
-# data = data*math.sqrt(5)+5
-# x = np.linspace(-10,15,10000)
-# dat2 = [Normal(i, 5, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for BoxMuller method: N=100, mean=5, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(BM1)
+data = data*math.sqrt(5)+5
+x = np.linspace(-10,15,10000)
+dat2 = [Normal(i, 5, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for BoxMuller method: N=100, mean=5, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(BM2)
-# data = data*math.sqrt(5)+5
-# x = np.linspace(-10,15,10000)
-# dat2 = [Normal(i, 5, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for BoxMuller method: N=10000, mean=5, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(BM2)
+data = data*math.sqrt(5)+5
+x = np.linspace(-10,15,10000)
+dat2 = [Normal(i, 5, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for BoxMuller method: N=10000, mean=5, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(MG1)
-# data = data*math.sqrt(5)+5
-# x = np.linspace(-10,15,10000)
-# dat2 = [Normal(i, 5, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for MargsaliaBray method: N=100, mean=5, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(MG1)
+data = data*math.sqrt(5)+5
+x = np.linspace(-10,15,10000)
+dat2 = [Normal(i, 5, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for MargsaliaBray method: N=100, mean=5, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
-# data = np.array(MG2)
-# data = data*math.sqrt(5)+5
-# x = np.linspace(-10,15,10000)
-# dat2 = [Normal(i, 5, 5) for i in x]
-# plt.plot(x, dat2)
-# plt.title("Histogram for MargsaliaBray method: N=10000, mean=5, var=5 ")
-# plt.hist(data, 100, density=True)
-# plt.show()
+data = np.array(MG2)
+data = data*math.sqrt(5)+5
+x = np.linspace(-10,15,10000)
+dat2 = [Normal(i, 5, 5) for i in x]
+plt.plot(x, dat2)
+plt.title("Histogram for MargsaliaBray method: N=10000, mean=5, var=5 ")
+plt.hist(data, 100, density=True)
+plt.show()
 
 print("Time taken for Box miller for 100 samples:", BMT1)
 print("Time taken for Margsalia and Bray for 100 samples:", MGT1)
